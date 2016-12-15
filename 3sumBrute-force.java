5807baf134940f7d117ec9898b8ac679702ce5eb
jinsiang <jinsiang2008@gmail.com>
2016-12-14 23:45:37 -0500

    L1 3-sum Brute-force

---
+++ 3sumBrute-force.java
@@ -0,0 +1,21 @@
+public class 3sum{
+	public static int count(int[] a){
+		int N = a.length;
+		int count = 0;
+		for(int i=0; i<N; i++){
+			for(int j = i+1; j < N; j++){
+				for(int k = j+1; k<N; k++){
+					if(a[i]+a[j]+a[k] == 0){
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	public static void main(){
+		int[] a = In.readInts();
+		StdOut.println(count(a));
+	}
+}
